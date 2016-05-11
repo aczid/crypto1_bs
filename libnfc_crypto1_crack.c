19b6add6c56c709a438aa81e2f8c10e4f6a21c19
Aram <aram@factorit.nl>
2016-05-11 23:26:08 +0200

    Write target key in filename correctly

--- libnfc_crypto1_crack.c
+++ libnfc_crypto1_crack.c
@@ -634,7 +634,7 @@ int main (int argc, const char * argv[]) {
     }
     
     char filename[21];
-    sprintf(filename, "0x%04x_%03u%s.txt", uid, target_block, ab_key == MC_AUTH_A ? "A" : "B");
+    sprintf(filename, "0x%04x_%03u%s.txt", uid, target_block, target_key == MC_AUTH_A ? "A" : "B");
     fp = fopen(filename, "wb");
 
     printf("Found tag with uid %04x, collecting nonces for key %s of block %u using known key %s %012"PRIx64" for block %u\n", uid, target_key == MC_AUTH_A ? "A" : "B", target_block, ab_key == MC_AUTH_A ? "A" : "B", known_key, for_block);
