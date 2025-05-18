129af73798953632e5bf3ef6fad189b7993d27b5
火山大隊長 <brian20020925@gmail.com>
2025-05-18 18:03:20 +0800

    Upgrade from Python 2 to Python 3
    Replace file() with open() and context managers
    Update string/bytes handling for Python 3 compatibility
    Convert raw_input() to input()

--- txttobin.py
+++ txttobin.py
@@ -1,29 +1,33 @@
 import sys
-data = file(sys.argv[1], 'rb').readlines()
+with open(sys.argv[1], 'rb') as f:
+    data = f.readlines()
 if sys.argv[1].endswith('.txt'):
     uid = sys.argv[1][:-4]
-
 def parity(x):
     return ("{0:08b}".format(x).count('1') & 1)
 
-uid=raw_input("uid> ")
-fp = file(uid+'_generated.bin','wb')
-fp.write(uid.decode('hex')+"\x00"*2)
-bits = 0
-pbits = 0
-for l in data:
-    if bits == 8:
-        fp.write(chr(pbits))
-        bits = 0
-        pbits = 0
-    hexbytes = filter(None, l.strip().split(' '))
-    bits += 4
-    for x in hexbytes:
-        b = int(x.replace('!', ''), 16)
-        p = parity(b)
-        if '!' not in x:
-            p^=1
-        pbits <<= 1
-        pbits |= p
-        fp.write(chr(b))
+uid = input("uid> ")
+uid_bytes = bytes.fromhex(uid)
 
+with open(uid + '_generated.bin', 'wb') as fp:
+    fp.write(uid_bytes + b"\x00" * 2)
+    bits = 0
+    pbits = 0
+    for l in data:
+        if bits == 8:
+            fp.write(bytes([pbits]))
+            bits = 0
+            pbits = 0
+        if isinstance(l, bytes):
+            l = l.decode('utf-8')
+        hexbytes = [x for x in l.strip().split(' ') if x]
+        
+        bits += 4
+        for x in hexbytes:
+            b = int(x.replace('!', ''), 16)
+            p = parity(b)
+            if '!' not in x:
+                p ^= 1
+            pbits <<= 1
+            pbits |= p
+            fp.write(bytes([b]))
